d09abdf62b0aa6b1008c1046bc794f7c5bb24137
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2023-12-17 15:07:22 +0700

    created PostShowPage

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,7 +1,22 @@
-export default async function PostShowPage() {
+import PostShow from '@/components/posts/post-show';
+import paths from '@/paths';
+import Link from 'next/link';
+
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  };
+}
+
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = params;
   return (
-    <div>
-      <h1>Post Show</h1>
+    <div className='space-y-3'>
+      <Link className='underline decoration-solid' href={paths.topicShow(slug)}>
+        {'< '}Back to {slug}
+      </Link>
+      <PostShow postId={postId} />
     </div>
   );
 }
