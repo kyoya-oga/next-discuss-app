1892263b51ae27f6702a6e27aa83e5df7c29b505
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2023-12-31 15:43:35 +0700

    added post show loading

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,9 +1,11 @@
 import CommentCreateForm from '@/components/comments/comment-create-form';
 import CommentList from '@/components/comments/comment-list';
 import PostShow from '@/components/posts/post-show';
+import PostShowLoading from '@/components/posts/post-show-loading';
 import { fetchCommentsByPostId } from '@/db/queries/comments';
 import paths from '@/paths';
 import Link from 'next/link';
+import { Suspense } from 'react';
 
 interface PostShowPageProps {
   params: {
@@ -19,7 +21,9 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       <Link className='underline decoration-solid' href={paths.topicShow(slug)}>
         {'< '}Back to {slug}
       </Link>
-      <PostShow postId={postId} />
+      <Suspense fallback={<PostShowLoading />}>
+        <PostShow postId={postId} />
+      </Suspense>
       <CommentCreateForm postId={postId} />
       <CommentList postId={postId} />
     </div>
